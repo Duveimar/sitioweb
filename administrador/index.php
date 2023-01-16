09809e618e6f77a02d5e04eb5802d1b95dfe584e
Duveimar <arevalomeloduveimar@gmail.com>
2023-01-15 22:33:22 -0500

    formulario de producto em

--- administrador/index.php
+++ administrador/index.php
@@ -1,12 +1,59 @@
-<!DOCTYPE html>
+<?php
+if($_POST){
+    header('location:inicio.php');  
+}
+?>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador del sitio web</title>
+    <title>Administrador deñ sitio MAD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    login
+
+    <div class="container">
+        <div class="row">
+
+        <div class="col-md-4">
+            
+        </div>
+            <div class="col-md-4">
+                <br/><br/><br/>
+
+        <div class="card">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+
+                <form method="POST" >
+
+                <div class = "form-group">
+                <label>Usuario</label>
+
+                <input type="text" class="form-control" name="usuario"  placeholder="Escrive tu usuario">
+
+            </div>
+                <div class="form-group">
+                <label >Contraseña</label>
+                <input type="password" class="form-control" name="contraseña" placeholder="Escive tu contraseña">
+                </div>
+            
+                <button type="submit" class="btn btn-primary">Entrar al administrador</button>
+                </form>
+            
+            
+        </div>
+
+            </div>
+            
+        </div>
+    </div>
+
 </body>
 </html>
--- template/cabecera.php
+++ template/cabecera.php
@@ -34,5 +34,5 @@
 
 
 <div class="container">
-    <br/>
+    <br/><br/> 
     <div class="row">
